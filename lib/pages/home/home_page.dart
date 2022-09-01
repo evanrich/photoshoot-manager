2f54aaf0d44e5a622c8b6326af3c6b7d3516bad1
venir.dev <luca@venir.dev>
2022-09-01 12:38:20 +0200

    HomePage now exploits PlaceholderPage

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -1,18 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:photoshoot_manager/pages/placeholder_page.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text(
-        'Photoshoot Manager - Home page',
-        style: TextStyle(
-          color: Colors.black,
-          fontSize: 18,
-        ),
-      ),
-    );
+    return const PlaceholderPage(title: 'Home page');
   }
 }
