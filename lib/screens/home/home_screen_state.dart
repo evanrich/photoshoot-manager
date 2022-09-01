f90bc22e89b279349aa6a21535b2a25dd8093250
venir.dev <luca@venir.dev>
2022-09-01 12:18:09 +0200

    Added screens to the enhanced enum state
    + added dummy widgets for it
    + added a placeholder page

--- lib/screens/home/home_screen_state.dart
+++ lib/screens/home/home_screen_state.dart
@@ -1,18 +1,41 @@
 // import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import 'package:flutter/material.dart';
+
 enum AppScreens {
-  home,
-  calendar,
-  map,
-  clients,
-  chat,
-  documents,
-  travels,
-  notifications,
-  settings;
+  home(PlaceholderPage()),
+  calendar(PlaceholderPage()),
+  map(PlaceholderPage()),
+  clients(PlaceholderPage()),
+  chat(PlaceholderPage()),
+  documents(PlaceholderPage()),
+  travels(PlaceholderPage()),
+  notifications(PlaceholderPage()),
+  settings(PlaceholderPage());
+
+  const AppScreens(this.screen);
+  final Widget screen;
 }
 
 // The following might not be useful atm and will probably be deleted
 // class AppScreensNotifier extends StateNotifier<AppScreens> {
 //   AppScreensNotifier() : super(AppScreens.home);
 // }
+
+// For unimplemented pages
+class PlaceholderPage extends StatelessWidget {
+  const PlaceholderPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text(
+        'Photoshoot Manager - Todo page',
+        style: TextStyle(
+          color: Colors.black,
+          fontSize: 18,
+        ),
+      ),
+    );
+  }
+}
