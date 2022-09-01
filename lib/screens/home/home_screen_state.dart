c25c6328acd3c24a58e4d1117082764bb167765a
venir.dev <luca@venir.dev>
2022-09-01 12:11:12 +0200

    Implemented a page state
    + added a enum representing such state

---
+++ lib/screens/home/home_screen_state.dart
@@ -0,0 +1,18 @@
+// import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+enum AppScreens {
+  home,
+  calendar,
+  map,
+  clients,
+  chat,
+  documents,
+  travels,
+  notifications,
+  settings;
+}
+
+// The following might not be useful atm and will probably be deleted
+// class AppScreensNotifier extends StateNotifier<AppScreens> {
+//   AppScreensNotifier() : super(AppScreens.home);
+// }
