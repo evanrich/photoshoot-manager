cec2673ade9b591709c3b44cdfef0b0af19e545b
Evan Richardson <evanrich81@gmail.com>
2022-09-11 02:23:01 -0700

    More progress on Menu

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'app_providers.dart';
-import 'components/drawer/menu.dart';
+import 'components/drawer/menu2.dart';
 
 void main() {
   runApp(const ProviderScope(child: MyApp()));
 }
+
+final GlobalKey<ScaffoldState> _key = GlobalKey(); // Create a key
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -22,22 +25,24 @@ class MyApp extends StatelessWidget {
         fontFamily: 'Roboto-Light',
       ),
       home: Scaffold(
+        key: _key, // Assign the key to Scaffold.
         backgroundColor: Colors.white,
+        drawer: const Drawer(child: Menu2()),
         appBar: AppBar(
           backgroundColor: const Color.fromRGBO(20, 20, 20, 1),
           elevation: 50,
           leading: IconButton(
-            icon: const Icon(Icons.menu),
+            icon: const Icon(Icons.menu, color: Colors.white60),
             tooltip: 'Menu Icon',
-            onPressed: () {},
+            onPressed: () => _key.currentState!.openDrawer(),
           ),
-          //systemOverlayStyle: SystemUiOverlayStyle.light,
+          systemOverlayStyle: SystemUiOverlayStyle.light,
         ),
         extendBodyBehindAppBar: true,
         body: SafeArea(
           child: Row(
             children: const [
-              Menu(),
+              //Menu(),
               Expanded(child: Center(child: _Pages())),
             ],
           ),
